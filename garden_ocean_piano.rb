bbccaeadc0353e6be81ccfbec58b90760a887821
kirstiann <136749752+kirstiann@users.noreply.github.com>
2023-06-27 01:11:58 +0700

    Refactor code for scalability

---
+++ garden_ocean_piano.rb
@@ -0,0 +1,98 @@
+class InnovationStation
+  attr_accessor :design, :innovate, :create
+
+  def initialize(design, innovate, create)
+    @design = design
+    @innovate = innovate
+    @create = create
+  end
+
+  def explore
+    puts "Design, innovate, and create at the Innovation Station!"
+  end
+
+  def reinvent_the_wheel
+    puts "Let's reinvent the wheel here at the Innovation Station!"
+  end
+
+  def design_prototype
+    puts "Design your own prototype at the Innovation Station!"
+  end
+
+  def test_solutions
+    puts "Test out your solutions at the Innovation Station!"
+  end 
+
+  def create_the_future
+    puts "Create the future here at the Innovation Station!"
+  end
+
+  def design_in_3d
+    puts "Design in 3D here at the Innovation Station!"
+  end
+
+  def prototype_and_test
+    puts "Prototype and test your ideas here at the Innovation Station!"
+  end
+
+  def build_on_existing_tech
+    puts "Build on existing technology here at the Innovation Station!"
+  end
+
+  def foster_innovation
+    puts "Foster innovation and imagination here at the Innovation Station!"
+  end
+
+  def come_up_with_new_ideas
+    puts "Come up with new ideas here at the Innovation Station!"
+  end
+
+  def share_your_ideas
+    puts "Share your ideas here at the Innovation Station!"
+  end
+
+  def make_it_happen
+    puts "Make it happen here at the Innovation Station!"
+  end
+
+  def think_outside_the_box
+    puts "Think outside the box here at the Innovation Station!"
+  end
+
+  def make_something_amazing
+    puts "Make something amazing here at the Innovation Station!"
+  end
+
+  def innovate_for_the_future
+    puts "Innovate for the future here at the Innovation Station!"
+  end
+
+  def explore_unconventional_solutions
+    puts "Explore unconventional solutions here at the Innovation Station!"
+  end
+
+  def explore_your_mind
+    puts "Explore your mind and find new ideas here at the Innovation Station!"
+  end
+
+  def collaborate_for_success
+    puts "Collaborate for success here at the Innovation Station!"
+  end
+
+  def spark_creativity
+    puts "Spark creativity and innovation here at the Innovation Station!"
+  end
+
+  def use_imagination
+    puts "Use imagination when designing products here at the Innovation Station!"
+  end
+
+  def build_from_scratch
+    puts "Build from scratch here at the Innovation Station!"
+  end
+
+  def innovate_for_the_better
+    puts "Innovate for the better here at the Innovation Station!"
+  end
+
+end
