b0958b9b0f775016df7eedd544dcea06255e6ccc
kirstiann <136749752+kirstiann@users.noreply.github.com>
2023-06-27 06:55:18 +0700

    Add support for different file formats

---
+++ elephant_garden_yellow.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Print out a welcome message
+	fmt.Println("Welcome to The Innovation Station!")
+
+	// Create loop to print the number of line of code
+	for i := 0; i < 2000; i++ {
+		fmt.Println(i, " line of code written")
+	}
+}
